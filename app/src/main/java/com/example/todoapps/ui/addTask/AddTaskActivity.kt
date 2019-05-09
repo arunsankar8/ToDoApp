d2275863b388fda05b85418d7bca7c6bc6ece20e
Arun Sankar S <arun.s@inapp.com>
2019-05-09 17:33:15 +0530

    Added databinding

--- app/src/main/java/com/example/todoapps/ui/addTask/AddTaskActivity.kt
+++ app/src/main/java/com/example/todoapps/ui/addTask/AddTaskActivity.kt
@@ -2,44 +2,54 @@ package com.example.todoapps.ui.addTask
 
 import android.app.Activity
 import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import com.example.todoapps.R
 import com.example.todoapps.database.entity.Tasks
-import kotlinx.android.synthetic.main.activity_add_task.*
+import com.example.todoapps.databinding.ActivityAddTaskBinding
 
 class AddTaskActivity : AppCompatActivity() {
-
+    lateinit var binding:ActivityAddTaskBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_task)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_add_task)
+        val buttonClicks=ButtonClick(binding,this)
+
+        binding.btnClick=buttonClicks
 
 
-
-        btnAddTask.setOnClickListener {
-
-            val task= Tasks()
-            if(etHeading.text.toString().trim().isNotEmpty()){
-                if(etTaskDesc.text.toString().trim().isNotEmpty()){
-                    task.taskName=etHeading.text.toString()
-                    task.taskDesc=etTaskDesc.text.toString()
-                    val intent= Intent()
-                    intent.putExtra("tasks",task)
-                    setResult(Activity.RESULT_OK,intent)
-                    finish()
-                }else{
-                    Snackbar.make(it,"Empty Description",Snackbar.LENGTH_SHORT)
-                }
-
-            }else{
-                Snackbar.make(it,"Empty name",Snackbar.LENGTH_SHORT)
-            }
-
-
-
-        }
 
 
     }
+
+    class ButtonClick(
+        val binding: ActivityAddTaskBinding,
+        val addTaskActivity: AddTaskActivity
+    ) {
+
+        fun AddButtonClick(){
+
+            val task= Tasks()
+            if(binding.etHeading.toString().trim().isNotEmpty()){
+                if(binding.etTaskDesc.text.toString().trim().isNotEmpty()){
+                    task.taskName=binding.etHeading.text.toString()
+                    task.taskDesc=binding.etTaskDesc.text.toString()
+                    val intent= Intent()
+                    intent.putExtra("tasks",task)
+                    addTaskActivity.setResult(Activity.RESULT_OK,intent)
+                    addTaskActivity.finish()
+                }else{
+                    //Snackbar.make(it,"Empty Description",Snackbar.LENGTH_SHORT)
+                }
+
+            }else{
+                //Snackbar.make(it,"Empty name",Snackbar.LENGTH_SHORT)
+            }
+        }
+
+
+
+    }
+
 }
